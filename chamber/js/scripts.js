248d8e03b18ed7bf4a0db120dcd8c59f4db7efb2
KevSJones <90716565+KevSJones@users.noreply.github.com>
2022-02-12 11:11:07 -0500

    dates

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -10,3 +10,24 @@ const x = document.getElementById(
   "hamburgerBtn"
 );
 x.onclick = toggleMenu;
+
+
+//  Reminder message
+const today = new Date();
+console.log(today);
+  const dayNumber = today.getDay();
+  console.log(dayNumber);
+
+  const element = document.getElementById("reminder");
+  
+  if (dayNumber == 6) {
+    element.classList.add("showme");
+  } else {
+    element.classList.add("hideme");
+  }
+
+  let date = new Date();
+  
+  document.getElementById(
+    "date"
+  ).textContent = date;
