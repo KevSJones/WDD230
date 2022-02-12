3991080f5121e774de5cee8b2aa1e885f3cff832
KevSJones <90716565+KevSJones@users.noreply.github.com>
2022-02-12 15:49:04 -0500

    responsive images and windchill javascript

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -10,7 +10,13 @@ const x = document.getElementById(
   "hamburgerBtn"
 );
 x.onclick = toggleMenu;
+// dates
 
+let day = new Date();
+let year = day.getFullYear();
+document.getElementById(
+  "currentYear"
+).textContent = year;
 
 //  Reminder message
 const today = new Date();
@@ -20,7 +26,7 @@ console.log(today);
 
   const element = document.getElementById("reminder");
   
-  if (dayNumber == 6) {
+  if (dayNumber == 1) {
     element.classList.add("showme");
   } else {
     element.classList.add("hideme");
@@ -31,3 +37,19 @@ console.log(today);
   document.getElementById(
     "date"
   ).textContent = date;
+
+  // wind chill 
+  const tempNumber = parseFloat(document.getElementById("temp").textContent);
+  console.log(tempNumber)
+  const windNumber = parseFloat(document.getElementById("wind").textContent);
+  console.log(windNumber)
+
+  let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(windNumber,0.16)) + (0.4275 * tempNumber * Math.pow(windNumber, 0.16));
+  windchill = Math.round(windchill);
+  console.log(windchill)
+
+  if(tempNumber <=50 && windNumber >3) {
+    document.getElementById("chill").textContent = "Wind Chill is "+windchill+"\xB0F";
+  } else {
+      document.getElementById("chill").textContent = "N/A"
+  }
