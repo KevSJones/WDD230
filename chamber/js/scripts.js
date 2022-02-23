aa6b2b160cb7a50942680f60f9d3ee90d563fd40
KevSJones <90716565+KevSJones@users.noreply.github.com>
2022-02-23 15:09:48 -0500

    discover page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -56,3 +56,35 @@ console.log(today);
   } else {
       document.getElementById("chill").textContent = "N/A"
   }
+  // lazy load
+  const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 2,
+    rootMargin: "0px 0px 5px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+};
