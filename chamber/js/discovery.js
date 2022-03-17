c86275c802e0aae11f5ffc89be4a1f1578f49fdb
KevSJones <90716565+KevSJones@users.noreply.github.com>
2022-03-17 11:28:50 -0400

    json

---
+++ chamber/js/discovery.js
@@ -0,0 +1,40 @@
+const discoveryjson = 'chamber/data.json';
+
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+  console.table(jsonObject);
+  const prophets = jsonObject['prophets'];
+  prophets.forEach(displayProphets);
+  });
+  
+
+  function displayProphets(prophet) {
+    let card = document.createElement('section');
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p");
+    let place = document.createElement("p");
+    let death = document.createElement("p");
+    let protrait = document.createElement('img');
+
+    h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+    p.innerHTML = `Date of birth: ${prophet.birthdate}`;
+    place.innerHTML = `Birthplace: ${prophet.birthplace}`;
+    death.innerHTML = `Date passed away: ${prophet.death}`;
+    protrait.setAttribute('src', prophet.imageurl);
+    protrait.setAttribute('alt',`Portrait of ${prophet.name} ${prophet.lastname}`);
+    protrait.setAttribute('loading', 'lazy');
+
+    card.appendChild(h2);
+    card.appendChild(p);
+    card.appendChild(place);
+    card.appendChild(death);
+    card.appendChild(protrait);
+
+    cards.appendChild(card);
+
+  }
