96918216f2c8363acc698234b58ccf2a8e53e934
KevSJones <90716565+KevSJones@users.noreply.github.com>
2022-03-20 14:51:42 -0400

    temp json

--- week10/js/scripts.js
+++ week10/js/scripts.js
@@ -20,25 +20,9 @@ fetch(apiURL)
     document.getElementById("weather_icon").src=icon_path;
     
 
- });
+ }); //end of "then" fat arrow function
 
 
- 
-function capitalixe(word) {
-  return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
-// wind chill 
-const tempNumber = parseFloat(document.getElementById("temp").textContent);
-console.log(tempNumber)
-const windNumber = parseFloat(document.getElementById("wind").textContent);
-console.log(windNumber)
 
-let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(windNumber,0.16)) + (0.4275 * tempNumber * Math.pow(windNumber, 0.16));
-windchill = Math.round(windchill);
-console.log(windchill)
 
-if(tempNumber <=50 && windNumber >3) {
-  document.getElementById("chill").textContent = "Wind Chill is "+windchill+"\xB0F";
-} else {
-    document.getElementById("chill").textContent = "N/A"
-}
-}
+
